acf587849b2aa4eb720774023b866e42ed69bd38
Maksymilian <maksymilianwojnowski@gmail.com>
2023-05-17 14:12:17 +0200

    Rearrange class structure, added team constructors

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,236 +2,12 @@ package org.example;
 
 import java.util.HashMap;
 import java.util.ArrayList;
-
-
-
+import java.util.Random;
 
 
 public class Main {
-    public class Team{
 
-
-        // Variables
-        String name;
-        Integer wins;
-        Integer losses;
-        Integer gamesPlayed;
-        Double PointsScoredTotal;
-        Double PointsLostTotal;
-        Double PointsScoredPerGame;
-        Double PointsLostPerGame;
-        Double NetDifference;
-        Double OffensiveRating;
-        Double DeffensiveRating;
-        HashMap<String, Player> players = new HashMap<String, Player>();
-
-
-        // Methods used to calculate offensive and defensive potential of certain teams
-
-        public Double calculateOffensiveRating(){
-            Double ortg=0.1; //placeholder value
-            return ortg;
-        }
-        public Double calculateDefensiveRating(){
-            Double drtg=0.1; //placeholder value
-            return drtg;
-        }
-
-        // Setters
-
-        public void setName(String name){
-            this.name = name;
-        }
-
-        public void setWins(int wins){
-            this.wins = wins;
-        }
-
-        public void setLosses(int Losses){
-            this.losses = losses;
-        }
-        // Getters
-        public String getName() {
-            return this.name;
-        }
-        public int getWins() {
-            return this.wins;
-        }
-        public int getLosses() {
-            return this.losses;
-        }
-
-
-        // Incrementers
-
-        public void addWin() {
-            wins = wins + 1;
-        }
-
-        public void addLoss() {
-            losses = losses + 1;
-        }
-
-
-
-
-
-
-    }
-
-    public class Player{
-
-        // Variables
-        String name;
-        String lastName;
-        Double points;
-        Double assists;
-        Double blocks;
-        Double steals;
-        Double xFactor;
-        String role;
-
-
-
-        // Constructors
-
-        public Player(){
-            this.name = "";
-            this.lastName = "";
-            this.points = -1.0;
-            this.assists = -1.0;
-            this.blocks = -1.0;
-            this.steals = -1.0;
-            this.xFactor = -1.0;
-            this.role = "";
-        }
-        public Player(String name, String lastName, Double points, Double assists, Double blocks, Double steals, Double xFactor, String role){
-            this.name = name;
-            this.lastName = lastName;
-            this.points = points;
-            this.assists = assists;
-            this.blocks = blocks;
-            this.steals = steals;
-            this.xFactor = xFactor;
-            this.role = role;
-        }
-
-
-
-        // Setters
-
-        public void setName(String name){
-            this.name = name;
-        }
-
-        public void setLastName(String lastName){
-            this.lastName = lastName;
-        }
-
-        public void setRole(String role){
-            this.role = role;
-        }
-
-        public void setPoints(Double points){
-            this.points = points;
-        }
-
-        public void setAssists(Double assists){
-            this.assists = assists;
-        }
-
-        public void setBlocks(Double blocks){
-            this.blocks = blocks;
-        }
-
-        public void setSteals(Double steals){
-            this.steals = steals;
-        }
-
-        public void setXfactor(Double xFactor){
-            this.xFactor = xFactor;
-        }
-
-        // Getters
-
-        public String getName(){
-            return this.name;
-        }
-
-        public String getLastName(){
-            return this.lastName;
-        }
-        public String getRole(){
-            return this.role;
-        }
-
-        public Double getPoints(){
-            return this.points;
-        }
-
-        public Double getAssists(){
-            return this.assists;
-        }
-
-        public Double getBlocks(){
-            return this.blocks;
-        }
-
-        public Double getSteals(){
-            return this.steals;
-        }
-
-        public Double getxFactor(){
-            return this.xFactor;
-        }
-
-
-    }
-
-
-
-    public class Tournament{
-
-        // Teams draw
-        public ArrayList<Team> Draw(){
-            ArrayList<Team> draw = new ArrayList<Team>();
-
-            return draw;
-        }
-
-
-
-    }
-    public class GameSimulator{
-
-        // Wyliczone od 0.9 do 1.1 * offensive rating i defensive rating
-        public Double FirstTeamRandomMultiplier(){
-            Double multiplier = -1.0;
-            return multiplier;
-        };
-        Double SecondTeamRandomMultiplier(){
-            Double multiplier = -1.0;
-            return multiplier;
-        };
-
-
-        public boolean getWinner(Team One, Team Two){
-            boolean winner=true;
-            // Conditional
-            return winner;
-        }
-
-
-
-    }
-
-    public class StatsReader{
-            public void Read(){
-
-            }
-    }
-
-    public class Menu{
+    class Menu{
 
         void showTeams(){
 
@@ -241,11 +17,10 @@ public class Main {
 
         }
 
-        void showStats(Player player){
 
-        }
 
     }
+
     public static void main(String[] args) {
 
 
@@ -256,3 +31,292 @@ public class Main {
 
     }
 }
+
+
+ class Team{
+
+
+    // Variables
+    String name;
+    Integer wins;
+    Integer losses;
+    Integer gamesPlayed;
+    Integer pointsScoredTotal;
+    Integer pointsLostTotal;
+    Double pointsScoredPerGame;
+    Double pointsLostPerGame;
+    Double netDifference;
+    Double offensiveRating;
+    Double defensiveRating;
+    HashMap<String, Player> players = new HashMap<String, Player>();
+
+
+     public Team(){
+         this.name = "";
+         this.wins = -1;
+         this.losses = -1;
+         this.gamesPlayed = -1;
+         this.pointsScoredTotal = -1;
+         this.pointsLostTotal = -1;
+         this.pointsScoredPerGame = -1.0;
+         this.pointsLostPerGame = -1.0;
+         this.netDifference = -1.0;
+         this.offensiveRating = -1.0;
+         this.defensiveRating = -1.0;
+     }
+     public Team(String name){
+         this.name = name;
+         this.wins = 0;
+         this.losses = 0;
+     }
+
+
+    // Methods used to calculate offensive and defensive potential of certain teams
+
+    public void calculateOffensiveRating(){
+        this.offensiveRating = 0.1; //placeholder value
+
+    }
+    public void calculateDefensiveRating(){
+        this.defensiveRating = 0.1; //placeholder value
+
+    }
+
+    public void calculateGamesPlayed(){
+        this.gamesPlayed = this.losses + this.wins;
+    }
+
+    public void calculateNetDifference(){
+        this.netDifference = Math.abs(this.offensiveRating - this.defensiveRating);
+    }
+
+    public void calculatePointsScoredPerGame(){
+        this.pointsScoredPerGame = Double.valueOf(this.pointsScoredTotal) / Double.valueOf(this.gamesPlayed);
+    }
+
+    public void calculatePointsLostPerGame(){
+        this.pointsLostPerGame = Double.valueOf(this.pointsLostTotal) / Double.valueOf(this.gamesPlayed);
+    }
+
+    // Methods to increment values
+
+    public void addWin() {
+        this.wins = this.wins + 1;
+    }
+
+    public void addLoss() {
+        this.losses = this.losses + 1;
+    }
+
+    public void addPlayer(String key, Player player){
+        this.players.put(key, player);
+    }
+
+    public void addPointsScoredTotal(Integer points){
+        this.pointsScoredTotal = this.pointsScoredTotal + points;
+    }
+
+    public void addPointsLostTotal(Integer points){
+        this.pointsLostTotal = this.pointsLostTotal + points;
+    }
+
+    // Setters
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setWins(int wins){
+        this.wins = wins;
+    }
+
+    public void setLosses(int Losses){
+        this.losses = losses;
+    }
+
+    // Getters
+    public String getName() {
+        return this.name;
+    }
+    public int getWins() {
+        return this.wins;
+    }
+    public int getLosses() {
+        return this.losses;
+    }
+
+    public Double getOffensiveRating(){
+        return this.offensiveRating;
+    }
+
+    public Double getDefensiveRating(){
+        return this.defensiveRating;
+    }
+
+    public Double getNetDifference(){
+        return this.netDifference;
+    }
+
+
+     class StatsReader{
+         public void Read(String fileName){
+
+         }
+     }
+
+
+     class Player{
+
+         // Variables
+         String name;
+         String lastName;
+         Double points;
+         Double assists;
+         Double blocks;
+         Double steals;
+         String role;
+
+
+
+         // Constructors
+
+         public Player(){
+             this.name = "";
+             this.lastName = "";
+             this.points = -1.0;
+             this.assists = -1.0;
+             this.blocks = -1.0;
+             this.steals = -1.0;
+             this.role = "";
+         }
+         public Player(String name, String lastName, Double points, Double assists, Double blocks, Double steals, String role){
+             this.name = name;
+             this.lastName = lastName;
+             this.points = points;
+             this.assists = assists;
+             this.blocks = blocks;
+             this.steals = steals;
+             this.role = role;
+         }
+
+
+
+         // Setters
+
+         public void setName(String name){
+             this.name = name;
+         }
+
+         public void setLastName(String lastName){
+             this.lastName = lastName;
+         }
+
+         public void setRole(String role){
+             this.role = role;
+         }
+
+         public void setPoints(Double points){
+             this.points = points;
+         }
+
+         public void setAssists(Double assists){
+             this.assists = assists;
+         }
+
+         public void setBlocks(Double blocks){
+             this.blocks = blocks;
+         }
+
+         public void setSteals(Double steals){
+             this.steals = steals;
+         }
+
+
+         // Getters
+
+         public String getName(){
+             return this.name;
+         }
+
+         public String getLastName(){
+             return this.lastName;
+         }
+         public String getRole(){
+             return this.role;
+         }
+
+         public Double getPoints(){
+             return this.points;
+         }
+
+         public Double getAssists(){
+             return this.assists;
+         }
+
+         public Double getBlocks(){
+             return this.blocks;
+         }
+
+         public Double getSteals(){
+             return this.steals;
+         }
+
+
+
+     }
+
+
+ }
+
+
+
+ class Tournament{
+
+    // Teams draw
+    public ArrayList<Team> Draw(){
+        ArrayList<Team> draw = new ArrayList<Team>();
+
+        return draw;
+    }
+
+     class GameSimulator{
+
+         public Double firstTeamRandomMultiplier;
+         public Double secondTeamRandomMultiplier;
+
+         public Integer firstTeamPointsScored;
+         public Integer secondTeamPointsScored;
+         public void calculateFirstTeamRandomMultiplier(){
+             Random random = new Random();
+             this.firstTeamRandomMultiplier = 0.8 + (1.1 - 0.8) * random.nextDouble();
+
+         };
+         public void calculateSecondTeamRandomMultiplier(){
+             Random random = new Random();
+             secondTeamRandomMultiplier = 0.8 + (1.1 - 0.8) * random.nextDouble();
+
+         };
+
+         public Integer getFirstTeamPointsScored(){
+             return this.firstTeamPointsScored;
+         }
+
+         public Integer getSecondTeamPointsScored(){
+             return this.secondTeamPointsScored;
+         }
+
+         public boolean getWinner(Team One, Team Two){
+             boolean winner=true;
+             // Conditional
+             return winner;
+         }
+
+
+
+     }
+
+}
+
+
+
+
